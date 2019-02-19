4ffbcc7603de732d1cc0e154f811c3e6dce0e184
Lauren <laurenribeiro06@gmail.com>
2019-02-19 22:40:24 +0000

    Added character counting functionality to the new tweet

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+  let maxChar = 140;
+
+  $('.textarea').on('input', function() {
+    let charCount = $(this).val().length;
+    let charLeft = maxChar - charCount;
+
+    $('.counter').text(charLeft);
+
+      if (charLeft < 0) {
+    $('.counter').css('color', 'red');
+    }
+  });
+
+});
