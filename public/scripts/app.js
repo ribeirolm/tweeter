ce2a39d323e7318753e1f0cdf7014c19c52c8a30
Lauren <laurenribeiro06@gmail.com>
2019-02-21 03:24:04 +0000

    iUpdated app with functionality and styling
    Added a tweet container to hold all submitted tweets.
    
    Added a hover state to tweets.
    
    Added functionality to dynamically render the tweet data.Used jQuery in generating a structure for submitted tweets.
    
    Used jQuery and AJAX to enable submission of tweets asynchronously.
    
    Used AJAX to enable fetching of data from the server.
    
    Added validation to the new tweet form to disallow empty tweets and tweets containing over 140 characters.
    
    Enabled fetching of tweets on submission of new tweet form so users can now see their submitted tweet loaded below the form.
    
    Added an escape function to prevent cross-site scripting based on user inputs.
    
    Added a button to toggle the view of the new tweet form. It will hide and reveal the new tweet form based on selection of the "Compose" button. The new tweet field will be in focus upon activation of the new tweet form.
    
    Created error messages to complement the new tweet form validation rules and display to the user the actions to take to successfully submit their tweet.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,6 +1,129 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
+// / Fake data taken from tweets.json
+// Test / driver code (temporary). Eventually will get this from the server.
+$(document).ready(function(){
+
+  // const data = [
+  //   {
+  //     "user": {
+  //       "name": "Newton",
+  //       "avatars": {
+  //         "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
+  //         "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
+  //         "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+  //       },
+  //       "handle": "@SirIsaac"
+  //     },
+  //     "content": {
+  //       "text": "If I have seen further it is by standing on the shoulders of giants"
+  //     },
+  //     "created_at": 1461116232227
+  //   },
+  //   {
+  //     "user": {
+  //       "name": "Descartes",
+  //       "avatars": {
+  //         "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
+  //         "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
+  //         "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
+  //       },
+  //       "handle": "@rd" },
+  //     "content": {
+  //       "text": "Je pense , donc je suis"
+  //     },
+  //     "created_at": 1461113959088
+  //   },
+  //   {
+  //     "user": {
+  //       "name": "Johann von Goethe",
+  //       "avatars": {
+  //         "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
+  //         "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
+  //         "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
+  //       },
+  //       "handle": "@johann49"
+  //     },
+  //     "content": {
+  //       "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
+  //     },
+  //     "created_at": 1461113796368
+  //   }
+  // ];
+
+  function escape(str) {
+    var div = document.createElement('div');
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  }
+
+  function renderTweets(tweets) {
+    // loops through tweets
+    tweets.forEach(function(el){
+      // calls createTweetElement for each tweet
+      let newTweet = createTweetElement(el);
+      // takes return value and appends it to the tweets container
+      $("#section-tweet").append(newTweet)
+    });
+  }
+
+  function createTweetElement(tweet) {
+    let $tweet = $('<article class="old-tweet">').append(`
+
+            <header class="art-header">
+              <img src=${escape(tweet.user.avatars.small)}>
+              <h3>${escape(tweet.user.name)}</h3>
+              <p>${escape(tweet.user.handle)}</p>
+            </header>
+            <p>${escape(tweet.content.text)}</p>
+            <footer >
+              <p> ${escape(tweet.created_at)}</p>
+              <span class="icons">
+                <i class="fas fa-flag"> </i>
+                <i class="fas fa-retweet"> </i>
+                <i class="fas fa-heart"> </i>
+              </span>
+              </footer>
+          </article> `)
+
+    return $tweet;
+  }
+
+  $("#newTweet").submit(function(event) {
+    event.preventDefault();
+    let $tweet = $(".textarea").val().length;
+        if ($tweet > 140) {
+          document.getElementById("err-longTweet").hidden = false;
+          document.getElementById("err-noTweet").hidden = true;
+
+        } else if($tweet === 0 || $tweet === null){
+          document.getElementById("err-longTweet").hidden = true;
+          document.getElementById("err-noTweet").hidden = false;
+
+        } else {
+        $("#err-longTweet").slideUp("slow");
+        $("#err-noTweet").slideUp("slow");
+        $.post( "/tweets/", $("#newTweet").serialize());
+        location.reload();
+      }
+
+  });
+
+  function loadTweets(){
+    $.ajax("/tweets", { method: 'GET' })
+    .then(function (tweet) {
+      renderTweets(tweet);
+      console.log(tweet);
+    });
+  }
+
+    loadTweets();
+
+  $("#compose-tweet").click(function() {
+    $(".new-tweet").slideToggle("slow");
+    $(".textarea").focus();
+  });
+
+
+});
+
+
 
