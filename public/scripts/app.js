166a15f92dcfcef03c37a6bccb4f7ff10f4428be
Lauren <laurenribeiro06@gmail.com>
2019-02-22 21:40:37 +0000

    Removed dead code and code used for debugging

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,56 +1,5 @@
-// / Fake data taken from tweets.json
-// Test / driver code (temporary). Eventually will get this from the server.
-
-// const timeFunction = require("./time-passed");
 
 $(document).ready(function(){
-  //Sample data used when testing:
-  //const data = [
-  //   {
-  //     "user": {
-  //       "name": "Newton",
-  //       "avatars": {
-  //         "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-  //         "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-  //         "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-  //       },
-  //       "handle": "@SirIsaac"
-  //     },
-  //     "content": {
-  //       "text": "If I have seen further it is by standing on the shoulders of giants"
-  //     },
-  //     "created_at": 1461116232227
-  //   },
-  //   {
-  //     "user": {
-  //       "name": "Descartes",
-  //       "avatars": {
-  //         "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-  //         "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-  //         "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-  //       },
-  //       "handle": "@rd" },
-  //     "content": {
-  //       "text": "Je pense , donc je suis"
-  //     },
-  //     "created_at": 1461113959088
-  //   },
-  //   {
-  //     "user": {
-  //       "name": "Johann von Goethe",
-  //       "avatars": {
-  //         "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-  //         "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-  //         "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
-  //       },
-  //       "handle": "@johann49"
-  //     },
-  //     "content": {
-  //       "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
-  //     },
-  //     "created_at": 1461113796368
-  //   }
-  // ];
 
 //Function to escape user input
   function escape(str) {
@@ -58,7 +7,6 @@ $(document).ready(function(){
     div.appendChild(document.createTextNode(str));
     return div.innerHTML;
   }
-
 
 //Function to render all tweets including recently submitted tweets
   function renderTweets(tweets) {
--- server/index.js
+++ server/index.js
@@ -11,9 +11,6 @@ const app           = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-// The in-memory database of tweets. It's a basic object with an array in it.
-// const db = require("./lib/in-memory-db");
-
 //The Mongo db known as Mongodb is the database of tweets now.
 const Mongodb = require("mongodb");
 const MONGODB_URI = "mongodb://localhost:27017/tweeter";
@@ -31,22 +28,6 @@ Mongodb.connect(MONGODB_URI, (err, db) => {
   console.log(`Connected to mongodb: ${MONGODB_URI}`);
 
 });
-
-// The `data-helpers` module provides an interface to the database of tweets.
-// This simple interface layer has a big benefit: we could switch out the
-// actual database it uses and see little to no changes elsewhere in the code
-// (hint hint).
-//
-// Because it exports a function that expects the `db` as a parameter, we can
-// require it and pass the `db` parameter immediately:
-
-
-// The `tweets-routes` module works similarly: we pass it the `DataHelpers` object
-// so it can define routes that use it to interact with the data layer.
-
-
-// Mount the tweets routes at the "/tweets" path prefix:
-
 
 app.listen(PORT, () => {
   console.log("Example app listening on port " + PORT);
